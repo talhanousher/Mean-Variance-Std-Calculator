768ff48164af8239cd2b25906e0c330fb780807b
Talha Nousher Ali <talha@sudofy.com>
2020-08-18 01:05:07 +0500

    calculate mean, variance and standard deviation of 3x3 array alsong axis and flattend  using numpy

---
+++ mean_var_std.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def calculate(l):
+    if(len(l) != 9):
+        raise ValueError('List must contain nine numbers.')
+    arr = np.array(l).reshape((3, 3))
+    calculations = {
+        'mean': [list(np.mean(arr, axis=0)), list(np.mean(arr, axis=1)), np.mean(arr)],
+        'variance': [list(np.var(arr, axis=0)), list(np.var(arr, axis=1)), np.var(arr)],
+        'standard deviation': [list(np.std(arr, axis=0)), list(np.std(arr, axis=1)), np.std(arr)],
+        'max': [list(np.max(arr, axis=0)), list(np.max(arr, axis=1)), np.max(arr)],
+        'min': [list(np.min(arr, axis=0)), list(np.min(arr, axis=1)), np.min(arr)],
+        'sum': [list(np.sum(arr, axis=0)), list(np.sum(arr, axis=1)), np.sum(arr)]
+    }
+
+    return calculations
